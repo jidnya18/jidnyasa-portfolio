75e18e3d0a86805f863e32cdff5719936c28951c
Jidnyasa <kuthejidnyasa@gmail.com>
2026-08-31 21:59:00 +0530

    Configure GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,18 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: "/jidnyasa-portfolio/",
 });
